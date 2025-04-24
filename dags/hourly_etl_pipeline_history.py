b6b971ec5605865e14f925312cf80111ba2a84e0
Ubuntu <ubuntu@de-arina-efremova-cn.mcs.local>
2025-04-24 13:37:12 +0000

    fix

--- dags/hourly_etl_pipeline_history.py
+++ dags/hourly_etl_pipeline_history.py
@@ -52,4 +52,4 @@ with DAG(
     start = DummyOperator(task_id="start",dag=dag)
     end = DummyOperator(task_id="end",dag=dag)
 
-    start >> stg_tasks >> [finalize_tasks] >> end 
+    start >> stg_tasks >> finalize_tasks >> end 
