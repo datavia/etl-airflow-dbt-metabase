0444ef1b5180e0ea8ab00aaf4fb70c672172b9fd
Alexander Zhukov <aszhukov.de@gmail.com>
2025-04-29 12:41:06 +0300

    feat: Добавлены справочники (seeds). Добавлены поля в витрину dm_sales: sale_geo_region_name_ru, clear_sale_page_before_ru и гео координаты: sale_geo_latitude, sale_geo_longitude. Обновлена документация

--- dbt/models/dm/dm_sales.sql
+++ dbt/models/dm/dm_sales.sql
@@ -1,23 +1,25 @@
-WITH unique_devices AS (
-    SELECT DISTINCT
+with unique_devices as (
+    select distinct
         click_id,
         os,
         os_name,
         device_type,
         user_custom_id
-    FROM {{ ref('ods_device_events') }}
+    from {{ ref('ods_device_events') }}
 ),
 
-unique_geo AS (
-    SELECT DISTINCT
+unique_geo as (
+    select distinct
         click_id,
         geo_country,
-        geo_region_name
-    FROM {{ ref('ods_geo_events') }}
+        geo_region_name,
+        geo_latitude,
+        geo_longitude
+    from {{ ref('ods_geo_events') }}
 ),
 
-cte_lead AS (
-    SELECT
+cte_lead as (
+    select
         obe.load_hour,
         event_timestamp,
         event_type,
@@ -38,50 +40,60 @@ cte_lead AS (
         device_type,
         geo_country,
         geo_region_name,
-        LEAD(page_url_path, 1) OVER w AS next_page_lead1,
-        LEAD(page_url_path, 2) OVER w AS next_page_lead2,
-        LEAD(page_url_path, 3) OVER w AS next_page_lead3,
-        LEAD(event_timestamp, 3) OVER w AS confirmation_timestamp
-    FROM {{ ref('ods_location_events') }} AS ole
-    INNER JOIN
-        {{ ref('ods_browser_events') }} AS obe
-        ON ole.event_id = obe.event_id
-    INNER JOIN unique_devices AS ud ON obe.click_id = ud.click_id
-    INNER JOIN unique_geo AS ug ON obe.click_id = ug.click_id
-    WHERE
+        geo_latitude,
+        geo_longitude,
+        LEAD(page_url_path, 1) over w as next_page_lead1,
+        LEAD(page_url_path, 2) over w as next_page_lead2,
+        LEAD(page_url_path, 3) over w as next_page_lead3,
+        LEAD(event_timestamp, 3) over w as confirmation_timestamp
+    from {{ ref('ods_location_events') }} as ole
+    inner join
+        {{ ref('ods_browser_events') }} as obe
+        on ole.event_id = obe.event_id
+    inner join unique_devices as ud on obe.click_id = ud.click_id
+    inner join unique_geo as ug on obe.click_id = ug.click_id
+    where
         ole.page_url_path != '/home'
         {% if is_incremental() %}
-            AND ole.load_hour
-            > (SELECT COALESCE(MAX(load_hour), '1900-01-01') FROM {{ this }})
-            AND obe.load_hour
-            > (SELECT COALESCE(MAX(load_hour), '1900-01-01') FROM {{ this }})
+            and ole.load_hour
+            > (select COALESCE(MAX(load_hour), '1900-01-01') from {{ this }})
+            and obe.load_hour
+            > (select COALESCE(MAX(load_hour), '1900-01-01') from {{ this }})
         {% endif %}
-    WINDOW w AS (
-        PARTITION BY user_custom_id
-        ORDER BY event_timestamp
+    window w as (
+        partition by user_custom_id
+        order by event_timestamp
     )
 )
 
-SELECT
+select
     load_hour,
-    user_custom_id AS sale_custom_id,
+    user_custom_id as sale_custom_id,
     event_timestamp,
-    confirmation_timestamp AS sale_timestamp,
-    1 AS sale_item_cnt,
-    browser_name AS sale_browser,
-    device_type AS sale_platform,
-    page_url_path AS sale_page_before,
-    utm_source AS sale_source,
-    utm_campaign AS sale_utm_campaign,
-    referer_url AS sale_referer_url,
-    referer_medium AS sale_referer_medium,
-    utm_content AS sale_utm_content,
-    geo_country AS sale_geo_country,
-    geo_region_name AS sale_geo_region_name,
-    EXTRACT(HOUR FROM confirmation_timestamp) AS sale_hour,
-    REPLACE(page_url_path, '/product_', '') AS clear_sale_page_before
-FROM cte_lead
-WHERE
+    confirmation_timestamp as sale_timestamp,
+    1 as sale_item_cnt,
+    browser_name as sale_browser,
+    device_type as sale_platform,
+    page_url_path as sale_page_before,
+    utm_source as sale_source,
+    utm_campaign as sale_utm_campaign,
+    referer_url as sale_referer_url,
+    referer_medium as sale_referer_medium,
+    utm_content as sale_utm_content,
+    geo_country as sale_geo_country,
+    geo_region_name as sale_geo_region_name,
+    geo_latitude as sale_geo_latitude,
+    geo_longitude as sale_geo_longitude,
+    EXTRACT(hour from confirmation_timestamp) as sale_hour,
+    REPLACE(page_url_path, '/product_', '') as clear_sale_page_before,
+    COALESCE(ic.city_ru, '') as sale_geo_region_name_ru,
+    COALESCE(i.item_ru, '') as clear_sale_page_before_ru
+from cte_lead
+left join {{ ref('italian_cities') }} as ic on ic.city_en = geo_region_name
+left join
+    {{ ref('items') }} as i
+    on i.item_en = REPLACE(page_url_path, '/product_', '')
+where
     (next_page_lead1, next_page_lead2, next_page_lead3)
     = ('/cart', '/payment', '/confirmation')
-    AND page_url_path NOT IN ('/payment', '/cart')
+    and page_url_path not in ('/payment', '/cart', '/confirmation')
--- dbt/models/ods/ods_geo_events.sql
+++ dbt/models/ods/ods_geo_events.sql
@@ -1,17 +1,17 @@
-select load_dttm load_hour,
-       (json_data->>'click_id')::uuid click_id,
-        (json_data->>'geo_latitude')::float geo_latitude,
-        (json_data->>'geo_longitude')::float geo_longitude,
-        json_data->>'geo_country' geo_country,
-        json_data->>'geo_timezone' geo_timezone,
-        json_data->>'geo_region_name' geo_region_name,
-        json_data->>'ip_address' ip_address
-  from {{ source ('stg', 'stg_geo_events') }}
+select
+    load_dttm as load_hour,
+    (json_data ->> 'click_id')::uuid as click_id,
+    (json_data ->> 'geo_latitude')::float as geo_latitude,
+    (json_data ->> 'geo_longitude')::float as geo_longitude,
+    json_data ->> 'geo_country' as geo_country,
+    json_data ->> 'geo_timezone' as geo_timezone,
+    json_data ->> 'geo_region_name' as geo_region_name,
+    json_data ->> 'ip_address' as ip_address
+from {{ source ('stg', 'stg_geo_events') }}
 
 {% if is_incremental() %}
-WHERE load_dttm > (select coalesce(max(load_hour), '1900-01-01')
- FROM {{ this }})
+    where load_dttm > (
+        select coalesce(max(load_hour), '1900-01-01')
+        from {{ this }}
+    )
 {% endif %}
-
-
-
